65762da15de901bc4e0469d1717b7ff2f6f80f23
Tristan Speleos <tristanspeleos@pop-os.localdomain>
2019-08-12 12:10:19 -0400

    Added task and event functionality for gwin

--- tkgfx/tkgfx.py
+++ tkgfx/tkgfx.py
@@ -31,18 +31,46 @@ class Vect2D(object):
         except Exception as e:
             raise ValueError("Vect2D given bad argument", e)
     
-    def __mult__(self, b):
-        vec = Vect2D(0,0)
+    def __sub__(self, b):
+        nx = -1
+        ny = -1
         try:
-            for val in b:
-                print(val)
+            nx = self.x - b.x
+            ny = self.y - b.y
+            return Vect2D(nx, ny)
         except:
             pass
-        return vec
+        try:
+            nx = self.x - b
+            ny = self.y - b
+            return Vect2D(nx, ny)
+        except Exception as e:
+            raise ValueError("Vect2D given bad argument", e)
+    
+
+    def __mul__(self, b):
+        try:
+            vec = Vect2D(0,0)
+            vec.x = (self.x * b[0][0]) + (self.y * b[1][0])
+            vec.y = (self.x * b[0][1]) + (self.y * b[1][1])
+            return vec
+        except:
+            pass
+        try:
+            vec = Vect2D(0,0)
+            vec.x = self.x * b
+            vec.y = self.y * b
+            return vec
+        except:
+            raise ValueError("Vect2D cannot multiply by {0}".format(b.__name__))
 
 
     def __str__(self):
         return str([self.x, self.y])
+
+    def copyFromVec(self, vect):
+        self.x = vect.x
+        self.y = vect.y
 
 
 class Matrix2x2(object):
@@ -76,6 +104,7 @@ class Matrix2x2(object):
             vis += "|\n"
         return vis
 
+
 class Transform(object):
     def __init__(self, width, height):
         self.x_max = width - 1
@@ -103,18 +132,48 @@ class Transform(object):
         vec.y += cy
         self.y_flip(vec)
 
+class TkTask(object):
+    def __init__(self, gwin, ms, callback, *args, **kw):
+        self.interval = ms
+        self.callback = callback
+        self.gwin = gwin
+        self.args = args
+        self.recurring = False
+        self.config(**kw)
+        self.stopcode = None
+        self.run()
 
+    def config(self, **kw):
+        if "recurring" in kw:
+            if kw["recurring"] == True:
+                self.recurring = True
+            elif kw["recurring"] == False:
+                self.recurring = False
+            else:
+                raise ValueError("recurring cannot be:" + kw["recurring"])
+
+    def run(self):
+        if self.recurring:
+            self.callback(*self.args)
+            self.stopcode = self.gwin.root.after(self.interval, self.run)
+        else:
+            self.stopcode = self.gwin.root.after(self.time, self.callback, *self.args)
+
+    def stop(self):
+        self.gwin.root.after_cancel(self.stopcode)
 
 
 class GWin(object):
     """GWin (Graphics Window) class intended to provide functionality
     heavily centered around custom draw graphics objects using
-    the tkinter canvas object."""
+    the tkinter canvas object. Subclass and override onUpdate() method."""
     def __init__(self, width=640, height=480, title="Window"):
         self.root = tk.Tk()
+        self.root.title(title)
         self.root.geometry("{0}x{1}".format(width, height))
         self.canvas = tk.Canvas(self.root, width=width, height=height)
         self.gobcont = []
+        self.tasks = []
         self.transform = Transform(width, height)
         self.transform.auto_setting = 0
 
@@ -122,7 +181,12 @@ class GWin(object):
         self.canvas.pack()
         self.canvas.config(bg="black", border=0, highlightthickness=0) 
 
+        # TK Loop vars
+        self.tk_interval = 16 #ms
+        self.tk_lastrun = 0
+
     def run(self):
+        self._tkloop()
         self.root.mainloop()
 
     def setTransScreen(self):
@@ -133,6 +197,30 @@ class GWin(object):
 
     def setTransOriginCenter(self):
         self.transform.auto_setting = 2
+
+    def _tkloop(self):
+        self.tk_lastrun = time.time()
+        self.onUpdate()
+        self.root.after(self.tk_interval, self._tkloop)
+
+    def onUpdate(self):
+        #override me in subclass
+        pass
+
+    def addTask(self, ms, callback, *args, **kw):
+        task = TkTask(ms, callback, *args, **kw)
+        self.tasks.append(task)
+        return task
+
+    def addEvent(self, etype, callback):
+        self.root.bind(etype, callback)
+
+    def removeTask(self, stopcode):
+        for task in self.tasks[:]:
+            if stopcode == task.stopcode:
+                task.stop()
+                self.tasks.remove(task)
+
 
 
 class GOB(object):
@@ -151,7 +239,7 @@ class GOB(object):
             self._gen_origin()
         self._draw()
         self.config(**self.options)
-        self.is_drawn = True
+        self.gwin.gobcont.append(self)
 
     def undraw(self):
         self._undraw()
@@ -174,9 +262,17 @@ class GOB(object):
                 can.itemconfig(self.id, outline=kw[key])
             else:
                 raise tk.TclError("unknown option \"-{0}\"".format(key))
-    
+        if len(kw) == 0:
+            self.default_config()
+
+    def default_config(self):
+        # Override me in subclass
+        pass
+
     def move(self, dx, dy):
         canv = self.gwin.canvas
+        self.origin += Vect2D(dx, dy)
+        self.center += Vect2D(dx, dy)
         for v in self.vects:
             v.x += dx
             v.y += dy
@@ -188,8 +284,15 @@ class GOB(object):
         pass
 
     def rotate(self, angle):
-
-        self._rotate()
+        canv = self.gwin.canvas
+        mat = Matrix2x2()
+        mat.rotation(angle)
+        for vec in self.vects:
+            vec.copyFromVec(vec - self.origin)
+            vec.copyFromVec(vec * mat)
+            vec.copyFromVec(vec + self.origin)
+        canv.coords(self.id, *self._unpack_vects())
+        
 
     def _rotate(self, angle):
         # Override me in subclass
@@ -205,7 +308,26 @@ class GOB(object):
     def _gen_origin(self):
         """Generates centered origin based on object's vectors.
         Also defines object's \"center.\" """
-        pass
+        c = Vect2D(0,0)
+        xmax = self.vects[0].x
+        xmin = xmax
+        ymax = self.vects[1].y
+        ymin = ymax
+        for v in self.vects:
+            if v.x > xmax:
+                xmax = v.x
+            elif v.x < xmin:
+                xmin = v.x
+            if v.y > ymax:
+                ymax = v.y
+            elif v.y < ymin:
+                ymin = v.y
+
+        xmid = (xmax - xmin) / 2 + xmin
+        ymid = (ymax - ymin) / 2 + ymin
+        self.origin = Vect2D(xmid, ymid)
+        self.center = Vect2D(xmid, ymid)
+
 
     def _unpack_vects(self):
         """Returns alternating list with alternating x and y values
@@ -229,6 +351,9 @@ class Point(GOB):
         canv = self.gwin.canvas
         self.id = canv.create_line(*self._unpack_vects())
 
+    def default_config(self):
+        self.config(fill="green")
+
 
 class Line(GOB):
     def __init__(self, gwin, v1, v2, **kw):
@@ -241,11 +366,16 @@ class Line(GOB):
         v2 = self.vects[1]
         self.id = canv.create_line(self._unpack_vects())
 
+    def default_config(self, **kw):
+        self.config(fill="green")
 
 class _BBox(GOB):
     def __init__(self, gwin, v1, v2, **kw):
         super(_BBox, self).__init__(gwin, **kw)
         self.vects.extend([v1,v2])
+
+    def default_config(self):
+        self.config(fill ="black", outline="green")
 
 
 class Rectangle(_BBox):
@@ -278,16 +408,11 @@ class Circle(_BBox):
 def main():
     gwin = GWin()
     gwin.setTransOriginCenter()
-    r = 200
-    for i in range(360):
-        x = math.cos(i) * r
-        y = math.sin(i) * r
-        p = Point(gwin, Vect2D(x,y), fill="white")
-        p.draw()
-    mm = Matrix2x2()
-    mm.rotation(30)
-    mv = Vect2D(0,0)
-    mv * mm
+    rect = Rectangle(gwin,Vect2D(-400,-400),Vect2D(400,400))
+    rect.draw()
+    oval = Oval(gwin, Vect2D(40,40), Vect2D(80,80))
+    oval.draw()
+    task = gwin.addTask(gwin, 1, oval.rotate, 1, recurring=True)
     gwin.run()
 
 if __name__ == "__main__":
